89fb5db055af69aa43cf9c47d7530fa4c8de26ce
yuki-inui <courage8210@gmail.com>
2022-03-13 21:20:28 +0900

    Century

---
+++ ABC200/A_Century/Century.js
@@ -0,0 +1,9 @@
+const main = (input) => {
+	const args = input.split(" ");
+	const N = parseInt(args, 10);
+
+	const century = Math.ceil(N / 100);
+	console.log(centuy);
+
+main(require('fs').readFileSync('/dev/stdin', 'utf8'));  
+
