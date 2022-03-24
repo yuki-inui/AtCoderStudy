5f68b464ee2ec34abefd1b42819f1af9abbb8244
yuki-inui <courage8210@gmail.com>
2022-03-24 21:48:04 +0900

    ThreeDice

---
+++ ABC202/A_ThreeDice/ThreeDice.js
@@ -0,0 +1,12 @@
+const main = (input) => {
+	const args = input.split(" ");
+	const N1 = parseInt(args, 10);
+	const N2 = parseInt(args[1], 10);
+	const N3 = parseInt(args[2], 10);
+
+	console.log(
+	Math.abs(N1 - 7) + Math.abs(N2 -7) + Math.abs(N3 -7));
+	
+	};
+main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+
