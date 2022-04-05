4c8702c309a739be45408e99d478007bb2936124
yuki-inui <courage8210@gmail.com>
2022-04-05 19:28:31 +0900

    hydrate

---
+++ ABC207/B_Hydrate/Hydrate.js
@@ -0,0 +1,9 @@
+const main = (input) => {
+  const args = input.split(" ");
+  const A = parseInt(args, 10);
+  const B = parseInt(args[1], 10);
+  const C = parseInt(args[2], 10);
+  const D = parseInt(args[3], 10);
+
+  
+}
