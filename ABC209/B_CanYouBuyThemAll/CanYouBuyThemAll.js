4570701192dfd4e2a17232633e050ddffffb7c10
yuki-inui <courage8210@gmail.com>
2022-04-16 18:35:10 +0900

    buy

---
+++ ABC209/B_CanYouBuyThemAll/CanYouBuyThemAll.js
@@ -0,0 +1,8 @@
+const main = (input) => {
+  const args = input.split("\n");
+  const N = parseInt(args, 10);
+  const X = parseInt(args[1], 10);
+  const A = args[2].split(" ").map((n) => parseInt(n, 10));
+
+  
+};
