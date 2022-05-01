a917004e9468884b94f6a0663450fd869af8ef83
yuki-inui <courage8210@gmail.com>
2022-05-01 12:58:18 +0900

    cals

--- ABC209/B_CanYouBuyThemAll/CanYouBuyThemAll.js
+++ ABC209/B_CanYouBuyThemAll/CanYouBuyThemAll.js
@@ -1,8 +1,29 @@
-const main = (input) => {
-  const args = input.split("\n");
-  const N = parseInt(args, 10);
-  const X = parseInt(args[1], 10);
-  const A = args[2].split(" ").map((n) => parseInt(n, 10));
+function main(input){
+  const data = input.replace(/\r/g,"").split("\n")
+  const data1 = data[0].split(" ")
+  const N = data1[0]
+  const X = data1[1]
+  const price = data[1].split(" ")
 
-  
-};
+  var calc = 0
+  var flag = 0;
+  for(let i = 0; i < N ; i++){
+      calc = calc + parseInt(price[i])
+      if( i % 2 == 1){
+          calc = calc - 1
+      }
+      if(calc > X){
+          flag = 1
+          break
+      }
+  }
+
+  if(flag == 0){
+      console.log("Yes")
+  }else{
+      console.log("No")
+  }
+
+  //console.log(N,X,price)////
+}
+main(require("fs").readFileSync("/dev/stdin","utf8"))
