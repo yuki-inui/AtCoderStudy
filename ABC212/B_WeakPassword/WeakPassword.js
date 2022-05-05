0c08ac29d8581fc5a80cd230d6b57ad2579d7f32
yuki-inui <courage8210@gmail.com>
2022-05-05 12:06:08 +0900

    latest

---
+++ ABC212/B_WeakPassword/WeakPassword.js
@@ -0,0 +1,8 @@
+const main = (input) => {
+  const args = input.split("");
+  const X = args.map((n) => parseInt(n, 10));
+
+  if(X === X[1] === X[2] === X[3]){
+    console.log("Weak");
+  }else if()
+};
