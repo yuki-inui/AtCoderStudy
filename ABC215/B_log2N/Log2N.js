dfbb839ecab0f235be4acdd5a60318a14f0773f2
yuki-inui <courage8210@gmail.com>
2022-06-05 15:37:13 +0900

    ABC215

--- ABC215/B_log2N/Log2N.js
+++ ABC215/B_log2N/Log2N.js
@@ -1,6 +1,13 @@
 const main = (input) => {
   const args  = input.split(" ");
   const N = parseInt(args, 10);
+  let ans;
 
-  
+  for (let k = 0; ; k++) {
+    if(2 ** k <= N) ans = k;
+    else break;
+  }
+  console.log(ans);
 };
+
+main(require('fs').readFileSync('/dev/stdin', 'utf8'));
